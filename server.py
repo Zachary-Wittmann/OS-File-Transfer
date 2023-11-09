f9cbad80a74b1351fd61a16a6804c3ef80e437d3
Zachary <zawittmann@miners.utep.edu>
2023-11-09 01:18:44 -0700

    11/9/23 Changes

--- server.py
+++ server.py
@@ -1,14 +1,33 @@
 #! /usr/bin/env python3
 
-import socket, sys, re, time
+import socket, os, sys, re, time
 
 def receive_file(connection, filename):
-    with open(filename, 'wb') as file:
+    folder_name = "transferred-files"
+    os.makedirs(folder_name, exist_ok=True)
+
+    file_path = os.path.join(folder_name, filename)
+
+    if os.path.isfile(file_path):
+        os.remove(file_path)
+        os.mknod(file_path)
+    else:
+        os.mknod(file_path)
+    
+    with open(file_path, 'wb') as file:
         while True:
             data = connection.recv(1024)
             if not data or data == b'EOF':
                 break
             file.write(data)
+
+def save_file(filename):
+    file_path = os.path.join(os.getcwd() + "/transferred-files", filename)
+    if os.path.isfile(file_path):
+        os.remove(file_path)
+        os.mknod(file_path)
+    else:
+        os.mknod(file_path)
 
 def frame_and_send_ack(connection, ack_message):
     ack_message = ack_message.encode()
@@ -31,12 +50,14 @@ def main():
     print(f"Receiving file: {received_filename}")
 
     receive_file(connection, received_filename)
+    #save_file(received_filename)
     print("File has been successfully received.")
 
     ack_message = "Successfully received. Acknowledgement sent"
     frame_and_send_ack(connection, ack_message)
     print("Acknowledgement sent")
 
+    connection.shutdown(socket.SHUT_WR)
     connection.close()
     server_socket.close()
 
